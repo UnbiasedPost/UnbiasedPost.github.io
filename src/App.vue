9ad952721ae9efbbe63022dc08971fa996efdcc0
Prabir Ghosh <mymail.prabir@gmail.com>
2021-05-24 22:17:14 +0530

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <AppBar/>
+    <v-main>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-main>    
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// @ is an alias to /src
+import AppBar from '@/components/AppBar.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AppBar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
 </style>
